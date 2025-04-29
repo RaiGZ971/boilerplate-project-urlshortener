764a3b9c025277ee194f31a783d6b0f3faa5bd10
Gian <108510905+RaiGZ971@users.noreply.github.com>
2025-04-30 04:08:05 +0800

    commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -9,6 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -19,6 +22,29 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+let url, id;
+let uniqueURL = () => Math.floor(Math.random() * 10000);
+
+app.post('/api/shorturl', (req, res, next) => {
+  try{
+    url = new URL(req.body.url);
+  } 
+  catch(err) {
+    return res.json({
+      error: "No short URL found for the given input"
+    })
+  }
+
+  id = uniqueURL();
+
+  next();
+
+}, (req, res) =>{
+  res.json({
+    original_url: url,
+    short_url: id
+  })
+})
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
